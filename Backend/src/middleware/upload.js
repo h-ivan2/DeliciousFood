302559bad4ff6826ee1959a5aeba1b5ae6886b65
h-ivan2 <hirwaivan884@gmail.com>
2026-05-06 14:00:00 +0200

    Modified file for better image upload

---
+++ Backend/src/middleware/upload.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../config/cloudinary');
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'delicious-food', // The folder in cloudinary where images will be stored
+    allowedFormats: ['jpg', 'png', 'jpeg', 'webp'],
+    transformation: [{ width: 1000, height: 1000, crop: 'limit' }] // basic transformation
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
