7919f9d121599a6b455ce72cbba8ca7187837507
h-ivan2 <hirwaivan884@gmail.com>
2026-05-21 21:56:38 +0200

    fix(review): recalculate average restaurant rating and count on review delete

--- Backend/src/models/review.model.js
+++ Backend/src/models/review.model.js
@@ -35,19 +35,32 @@ const reviewSchema = new mongoose.Schema({
 reviewSchema.index({ customer: 1, restaurant: 1 }, { unique: true });
 
 
-reviewSchema.post('save', async function () {
+reviewSchema.statics.calculateAverageRating = async function (restaurantId) {
   const Restaurant = require('./restaurant.model');
-  const stats = await this.constructor.aggregate([
-    { $match: { restaurant: this.restaurant } },
+  const stats = await this.aggregate([
+    { $match: { restaurant: restaurantId } },
     { $group: { _id: '$restaurant', avgRating: { $avg: '$rating' }, count: { $sum: 1 } } },
   ]);
 
   if (stats.length > 0) {
-    await Restaurant.findByIdAndUpdate(this.restaurant, {
+    await Restaurant.findByIdAndUpdate(restaurantId, {
       rating:      Math.round(stats[0].avgRating * 10) / 10,
       reviewCount: stats[0].count,
     });
+  } else {
+    await Restaurant.findByIdAndUpdate(restaurantId, {
+      rating:      0,
+      reviewCount: 0,
+    });
   }
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calculateAverageRating(this.restaurant);
+});
+
+reviewSchema.post('deleteOne', { document: true, query: false }, function () {
+  this.constructor.calculateAverageRating(this.restaurant);
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
