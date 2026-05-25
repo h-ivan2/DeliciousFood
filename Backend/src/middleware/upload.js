fd0c78e01c34ad28aa01dcb6edff7aff71b1adef
h-ivan2 <hirwaivan884@gmail.com>
2026-05-25 22:33:04 +0200

    Formatted file for better code visibility

--- Backend/src/middleware/upload.js
+++ Backend/src/middleware/upload.js
@@ -1,14 +1,14 @@
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinary');
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'delicious-food', // The folder in cloudinary where images will be stored
-    allowedFormats: ['jpg', 'png', 'jpeg', 'webp'],
-    transformation: [{ width: 1000, height: 1000, crop: 'limit' }] // basic transformation
-  }
+    folder: "delicious-food", // The folder in cloudinary where images will be stored
+    allowedFormats: ["jpg", "png", "jpeg", "webp"],
+    transformation: [{ width: 1000, height: 1000, crop: "limit" }], // basic transformation
+  },
 });
 
 const upload = multer({ storage: storage });
