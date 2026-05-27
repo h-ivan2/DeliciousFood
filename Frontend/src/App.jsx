52ced381a3c0b4b0888079aa4bc95295c81dd70e
h-ivan2 <hirwaivan884@gmail.com>
2026-05-27 21:55:02 +0200

    Added Super admin page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,8 @@ import{BrowserRouter,Routes,Route} from 'react-router-dom';
 import{ThemeProvider} from './context/ThemeContext';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
-import Signup  from './pages/Signup'
+import Signup  from './pages/Signup';
+import AdminDashboard from './pages/AdminDashboard';
 
 
 export default function App(){
@@ -13,6 +14,7 @@ export default function App(){
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/Login' element={<Login/>}/>
         <Route path='/Signup' element={<Signup/>}/>
+        <Route path='/admin' element={<AdminDashboard/>}/>
       </Routes>
       </BrowserRouter>
     </ThemeProvider>
